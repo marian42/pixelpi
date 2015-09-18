fcfc375baebd240fe2e52487f12f7cb089268cc8
Marian <mail@marian42.de>
2015-09-18 19:54:27 +0200

    Use number keys for virtual gamepad buttons

--- gamepad/virtualgamepad.py
+++ gamepad/virtualgamepad.py
@@ -12,6 +12,23 @@ class VirtualGamepad:
 	RIGHT = 14
 	
 	btn_count = 15
+
+	keycode_list = [
+		(pygame.K_LEFT, LEFT),
+		(pygame.K_RIGHT, RIGHT),
+		(pygame.K_UP, UP),
+		(pygame.K_DOWN, UP),
+		(pygame.K_0, 10),
+		(pygame.K_1, 1),
+		(pygame.K_2, 2),
+		(pygame.K_3, 3),
+		(pygame.K_4, 4),
+		(pygame.K_5, 5),
+		(pygame.K_6, 6),
+		(pygame.K_7, 7),
+		(pygame.K_8, 8),
+		(pygame.K_9, 9)
+	]
 	
 	def __init__(self, verbose = False):
 		self.verbose = verbose
@@ -38,14 +55,9 @@ class VirtualGamepad:
 			self.on_release(btn)
 
 	def keycode_to_int(self, keycode):
-		if (keycode == pygame.K_LEFT):
-			return self.LEFT
-		if (keycode == pygame.K_RIGHT):
-			return self.RIGHT
-		if (keycode == pygame.K_UP):
-			return self.UP
-		if (keycode == pygame.K_DOWN):
-			return self.DOWN
+		for relation in self.keycode_list:
+			if (relation[0] == keycode):
+				return relation[1]
 
 			
 	def consume_event(self, event):
