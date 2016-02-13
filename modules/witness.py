14cc21e20fbfccece6266d34155a6945e890f365
Marian <mail@marian42.de>
2016-02-13 10:49:29 +0100

    Highlight path errors for wrong solutions

--- modules/witness.py
+++ modules/witness.py
@@ -18,11 +18,18 @@ class MandatoryNode(AbstractPuzzleFeature):
 		self.node = node
 		self.puzzle = puzzle
 
+	def get_screen_position(self):
+		return Point(self.puzzle.offset.x + self.node.x * self.puzzle.cell_size, self.puzzle.offset.y + self.node.y * self.puzzle.cell_size)
+
 	def check(self, path):
-		return path.contains_node(self.node)
+		result = path.contains_node(self.node)
+		if not result:
+			path.errors.append(self.get_screen_position())
+		return result
 
 	def draw(self, screen):
-		screen.pixel[self.puzzle.offset.x + self.node.x * self.puzzle.cell_size][self.puzzle.offset.y + self.node.y * self.puzzle.cell_size] = darken_color(self.puzzle.line_color, 0.1)
+		pos = self.get_screen_position()
+		screen.pixel[pos.x][pos.y] = darken_color(self.puzzle.line_color, 0.1)
 
 class ForbiddenEdge(AbstractPuzzleFeature):
 	def __init__(self, puzzle, node1, node2):
@@ -44,17 +51,29 @@ class MandatoryEdge(AbstractPuzzleFeature):
 		self.node1 = node1
 		self.node2 = node2
 
+	def get_screen_position(self):
+		return Point(self.puzzle.offset.x + self.node1.x * self.puzzle.cell_size + (self.node2.x - self.node1.x), self.puzzle.offset.y + self.node1.y * self.puzzle.cell_size + (self.node2.y - self.node1.y))
+
 	def check(self, path):
-		return path.contains_edge(self.node1, self.node2)
+		result = path.contains_edge(self.node1, self.node2)
+		if not result:
+			path.errors.append(self.get_screen_position())
+		return result
 
 	def draw(self, screen):
-		position = Point(self.puzzle.offset.x + self.node1.x * self.puzzle.cell_size + (self.node2.x - self.node1.x), self.puzzle.offset.y + self.node1.y * self.puzzle.cell_size + (self.node2.y - self.node1.y))
-		screen.pixel[position.x][position.y] = darken_color(self.puzzle.line_color, 0.1)
+		pos = self.get_screen_position()
+		screen.pixel[pos.x][pos.y] = darken_color(self.puzzle.line_color, 0.1)
 
 class ColorBlocks(AbstractPuzzleFeature):
 	def __init__(self, puzzle):
 		self.puzzle = puzzle
 		self.colors = [[None for y in range(puzzle.height - 1)] for x in range(puzzle.width -1)]
+
+	def mark_error(self, path, x, y):
+		for a in range(self.puzzle.cell_size - 1):
+			for b in range(self.puzzle.cell_size - 1):
+				position = Point(self.puzzle.offset.x + x * self.puzzle.cell_size + 1 + a, self.puzzle.offset.y + y * self.puzzle.cell_size + 1 + b)
+				path.errors.append(position)
 
 	def check(self, path):
 		zones = path.get_zones()
@@ -66,6 +85,15 @@ class ColorBlocks(AbstractPuzzleFeature):
 				if self.colors[x][y] != None:
 					if zones[x][y] in zone_color_dict:
 						if zone_color_dict[zones[x][y]] != self.colors[x][y]:
+							self.mark_error(path, x, y)
+
+							marked_zone_block = False
+							for u in range(self.puzzle.width - 1):
+								for v in range(self.puzzle.height - 1):
+									if not marked_zone_block and zones[u][v] == zones[x][y] and self.colors[u][v] == zone_color_dict[zones[x][y]]:
+										self.mark_error(path, u, v)
+										marked_zone_block = True
+
 							return False
 					else:
 						zone_color_dict[zones[x][y]] = self.colors[x][y]
@@ -224,6 +252,7 @@ class Path:
 
 		self.wrong = False
 		self.wrong_since = None
+		self.errors = []
 
 	def animate_undo(self):
 		self.offset_end = time.clock()
@@ -327,6 +356,10 @@ class Path:
 						color = blend_colors(self.screen.pixel[pos.x][pos.y], color, opacity)
 
 					self.screen.pixel[pos.x][pos.y] = color
+
+		if self.wrong:
+			for error in self.errors:
+				self.screen.pixel[error.x][error.y] = blend_colors(self.screen.pixel[error.x][error.y], Color(255, 0, 0), math.sin(time.clock() * 8) ** 2)
 
 	def contains_node(self, point):
 		for step in self.steps:
@@ -509,5 +542,4 @@ class WitnessGame(Module):
 	def new_game(self):
 		self.puzzle = Puzzle(self.screen)
 		self.path = Path(self.puzzle, self.screen)
-		self.errors = []
 			
